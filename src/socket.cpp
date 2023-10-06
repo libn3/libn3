6c47309e1dfe8dc0536369c9ea2497ebaa27564d
John Agapeyev <jagapeyev@gmail.com>
2023-10-05 21:23:31 -0700

    Finished implementing basic writev wrapper

--- src/socket.cpp
+++ src/socket.cpp
@@ -1,5 +1,7 @@
+#include <algorithm>
 #include <arpa/inet.h>
 #include <cassert>
+#include <climits>
 #include <cstdint>
 #include <expected>
 #include <fcntl.h>
@@ -12,10 +14,12 @@
 #include <linux/socket.h>
 #include <linux/tcp.h>
 #include <linux/udp.h>
+#include <memory>
 #include <netdb.h>
 #include <span>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/uio.h>
 #include <unistd.h>
 #include <utility>
 #include <vector>
@@ -384,20 +388,34 @@ namespace n3 { namespace net { namespace linux {
 
     std::expected<size_t, error::code> socket::write_vectored(
             const std::span<const std::span<std::byte>> bufs) const noexcept {
-        const auto ret = writev(this->sock, level, option, option_buf.data(), &optlen);
+        //Validate assumption about the length of the caller
+        assert(bufs.size() <= IOV_MAX);
+
+        std::array<struct iovec, IOV_MAX> converted_bufs;
+        //Convert spans to iovecs by copying to a stack buffer
+        //There's probably a way of converting things without a copy...
+        std::transform(bufs.cbegin(), bufs.cend(), converted_bufs.begin(), [](const auto vec) {
+            return iovec{.iov_base = vec.data(), .iov_len = vec.size_bytes()};
+        });
+
+        const auto ret = writev(this->sock, converted_bufs.data(), bufs.size());
         if (ret == -1) {
             return std::unexpected(error::get_error_code_from_errno(errno));
         }
-        assert(optlen <= option_buf.size_bytes());
-        return option_buf.first(optlen);
+        assert(ret >= 0);
+        return ret;
     }
 
     std::expected<size_t, error::code> socket::send(const std::span<std::byte> buf) const noexcept {
-        const auto buf_vec = std::span<const std::span<std::byte>, 1>{&buf, 1zu};
+        const auto buf_vec = std::span<const std::span<std::byte>, 1>{std::addressof(buf), 1zu};
         return write_vectored(std::move(buf_vec));
     }
     std::expected<size_t, error::code> socket::send(
             const std::span<std::span<std::byte>> bufs) const noexcept {
+        //TODO: Checked at runtime, might be a better way of handling this?
+        if (bufs.size() > IOV_MAX) {
+            return std::unexpected(error::get_error_code_from_errno(EINVAL));
+        }
         return write_vectored(bufs);
     }
 }}} // namespace n3::net::linux
