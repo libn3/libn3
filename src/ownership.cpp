67367ca9a6688399f18719f3654319ca1eafe753
John Agapeyev <jagapeyev@gmail.com>
2024-02-04 12:46:09 -0800

    Some trivial static asserts to verify that the move only type is actually movable for some reasonable uses

--- src/ownership.cpp
+++ src/ownership.cpp
@@ -1,7 +1,20 @@
+#include <functional>
+#include <memory>
 #include <optional>
+#include <type_traits>
+#include <vector>
 
 #include "ownership.h"
 
 namespace n3 {
+
+//Some static asserts to verify that the MoveOnly type works with some reasonable types
+static_assert(std::movable<MoveOnly<int>>);
+static_assert(std::movable<MoveOnly<std::unique_ptr<int>>>);
+static_assert(std::movable<MoveOnly<std::shared_ptr<int>>>);
+static_assert(std::movable<MoveOnly<std::function<void(int)>>>);
+static_assert(std::movable<MoveOnly<void (*)(int)>>);
+static_assert(std::movable<MoveOnly<std::vector<int>>>);
+static_assert(std::movable<MoveOnly<std::optional<int>>>);
 
 }; // namespace n3
